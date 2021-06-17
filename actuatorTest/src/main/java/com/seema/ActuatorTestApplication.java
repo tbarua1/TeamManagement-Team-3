a3f6ea65a9de92f3834ff13fd44b99efca37b8ba
Seema Kumari <seemakmr052@gmail.com>
2021-06-17 13:33:23 +0530

    this is actuator project

---
+++ actuatorTest/src/main/java/com/seema/ActuatorTestApplication.java
@@ -0,0 +1,17 @@
+package com.seema;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorTestApplication {
+	
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorTestApplication.class, args);
+		
+	}
+
+}
---
+++ actuatorTest/src/test/java/com/seema/ActuatorTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
